688bc092aa0ff5ffb0409a0d11b4e11e09a1a017
jonschlinkert <dev@sellside.com>
2014-11-01 23:18:24 -0400

    clean up, get tests passing

--- index.js
+++ index.js
@@ -8,88 +8,52 @@
 'use strict';
 
 var isObject = require('isobject');
+var Tokens = require('preserve');
 var get = require('get-value');
 
-// module.exports = function set(o, path, value, fn) {
-//   if (!path) {
-//     return o;
-//   }
-
-//   var parts = path.split('.');
-//   var len = parts.length;
-
-//   for (var i = 0; i < len; i++) {
-//     var key = parts[i];
-//     if (!o[key]) {
-//       o[key] = {};
-//     }
-//     o = o[key];
-//   }
-//   return o;
-// };
-
-// module.exports = function set(o, prop, value, cb) {
-//   var arr = [].slice.call(arguments);
-//   if (o == null || !isObject(o)) {
-//     return {};
-//   }
-//   if (prop == null) {
-//     return o;
-//   }
-
-//   if (typeof cb === 'function') {
-//     fn = cb;
-//   }
-
-//   var path = fn(prop);
-//   var last = path.pop();
-//   var len = path.length;
-
-//   o = get(o, prop, fn) || {};
-
-//   if (o && typeof o === 'object') {
-//     return (o[last] = value);
-//   }
-// };
-
-// Set a deeply-nested property in an object, creating intermediary objects
-// as we go.
-module.exports = function set(obj, parts, value) {
-  parts = parts.split('.');
-  var prop = parts.pop();
-  obj = get(obj, parts);
-
-  console.log(obj)
-  if (obj && typeof obj === 'object') {
-    return (obj[prop] = value);
+module.exports = function set(obj, prop, val) {
+  if (prop == null) {
+    return obj;
   }
+
+  var tokens = new Tokens(/\\\./g);
+  prop = tokens.before(prop);
+
+  var parts = (/^([\s\S]+)\.([\s\S]+)$/).exec(prop);
+  if (parts) {
+    create(obj, parts[1])[parts[2]] = val;
+  } else {
+    prop = tokens.after(prop).replace(/\\\./g, '.');
+    obj[prop] = val;
+    return obj;
+  }
+  return obj;
 };
 
-// function fn(str) {
-//   return str.split('.');
-// }
-
-function fn(str) {
-  var tok = 'xzvZvzx'; // scrabble anyone?
-  var path = repl(str, '\u005C.', tok);
-  return path.split('.').map(function (seg) {
-    return repl(seg, tok, '.');
+function create(obj, path) {
+  if (!path) {
+    return obj;
+  }
+  forEach(path.split('.'), function (key) {
+    if (!obj[key] || !isObject(obj[key])) {
+      obj[key] = {};
+    }
+    obj = obj[key];
   });
+  return obj;
 }
 
-function repl(str, oldVal, newVal) {
-  str = String(str);
-  oldVal = String(oldVal);
-  newVal = String(newVal);
-
-  var pos = str.indexOf(oldVal);
-  var len = oldVal.length;
-  var prev;
-
-  while (pos !== -1) {
-    prev = str.substr(pos, len);
-    str = str.replace(prev, newVal);
-    pos = str.indexOf(prev);
+function forEach(arr, fn, thisArg) {
+  if (arr == null) {
+    return;
   }
-  return str;
+
+  var len = arr.length;
+  var i = -1;
+
+  while (++i < len) {
+    if (fn.call(thisArg, arr[i], i, arr) === false) {
+      break;
+    }
+  }
 }
--- test.js
+++ test.js
@@ -11,30 +11,47 @@ var should = require('should');
 var set = require('./');
 
 describe('set', function() {
-  it('should use property paths to set nested values from the source object.', function () {
-    var fixture = {
-      a: {locals : {name: {first: 'Brian'}}},
-      b: {locals : {name: {last: 'Woodward'}}}
-    };
-    set(fixture, 'a.locals.name').should.eql({first: 'Brian'});
-    set(fixture, 'b.locals.name').should.eql({last: 'Woodward'});
-    set(fixture, 'b.locals.name.last').should.eql('Woodward');
+  it('should create a nested property if it does not already exist:', function() {
+    var o = {};
+    set(o, 'a.b', 'c');
+    o.a.b.should.equal('c');
   });
 
-  it('should return an empty object if the path is not found', function () {
+  it('should not create a nested property if it does already exist:', function() {
+    var first = {name: 'Halle'};
+    var o = {a: first };
+    set(o, 'a.b', 'c');
+    o.a.b.should.equal('c');
+    o.a.should.equal(first);
+    o.a.name.should.equal('Halle');
+  });
+
+  it('shold support immediate properties:', function() {
+    var o = {};
+    set(o, 'a', 'b');
+    o.a.should.equal('b');
+  });
+
+  it('should use property paths to set nested values from the source object.', function () {
+    var o = {};
+    set(o, 'a.locals.name', {first: 'Brian'});
+    set(o, 'b.locals.name', {last: 'Woodward'});
+    set(o, 'b.locals.name.last', 'Woodward');
+    o.should.eql({ a: {locals: {name: { first: 'Brian' }} }, b: {locals: {name: { last: 'Woodward' }} }});
+  });
+
+  it('should add the property even if a value is not defined:', function () {
     var fixture = {};
-    set(fixture, 'a.locals.name').should.eql({});
-    set(fixture, 'b.locals.name').should.eql({});
+    set(fixture, 'a.locals.name').should.eql({a: {locals: { name: undefined }}});
+    set(fixture, 'b.locals.name').should.eql({b: {locals: { name: undefined }}, a: {locals: { name: undefined }}});
   });
 
   it('should set the specified property.', function () {
-    set({a: 'aaa', b: 'b'}, 'a').should.eql('aaa');
-    set({first: 'Jon', last: 'Schlinkert'}, 'first').should.eql('Jon');
-    set({locals: {a: 'a'}, options: {b: 'b'}}, 'locals').should.eql({a: 'a'});
+    set({a: 'aaa', b: 'b'}, 'a', 'bbb').should.eql({a: 'bbb', b: 'b'});
   });
 
   it('should set a value only.', function () {
-    set({a: 'a', b: {c: 'd'}}, 'a').should.eql('a');
+    set({a: 'a', b: {c: 'd'}}, 'b.c.d', 'eee').should.eql({a: 'a', b: {c: {d: 'eee'}}});
   });
 
   it('should return the entire object if no property is passed.', function () {
@@ -42,37 +59,6 @@ describe('set', function() {
   });
 
   it('should set a value only.', function () {
-    set({a: 'a', b: {c: 'd'}}, 'b.c').should.eql('d');
-  });
-
-  it('should set the value of a deeply nested property.', function () {
-    set({a: {b: 'c', c: {d: 'e', e: 'f', g: {h: 'i'}}}}, 'a.c.g.h').should.eql('i');
-  });
-
-  it('should return an empty object if the first value is null.', function () {
-    set(null, 'a.c.g.h').should.eql({});
-  });
-});
-
-describe('set', function() {
-  var obj = {};
-
-  it('should return immediate property value.', function () {
-    set(obj, 'a', 1).should.equal(1);
-  });
-  it('should set property value.', function () {
-    obj.a.should.equal(1);
-  });
-  it('should return nested property value.', function () {
-    set(obj, 'b.c.d', 1).should.equal(1);
-  });
-  it('should set property value.', function () {
-    obj.b.c.d.should.equal(1);
-  });
-  it('should literal backslash should escape period in property name.', function () {
-    set(obj, 'e\\.f\\.g', 1);
-    console.log(obj)
-    obj.should.equal(1);
-    obj['e.f.g'].should.equal(1);
+    set({a: 'a', b: {c: 'd'}}, 'b.c').should.eql({a: 'a', b: {c: undefined}});
   });
 });
