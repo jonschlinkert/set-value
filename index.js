d2c35ed6c3ddbf95bf13463b8c516970790185f1
jonschlinkert <jon.schlinkert@sellside.com>
2018-03-05 18:37:46 -0500

    refactor, again

--- index.js
+++ index.js
@@ -1,51 +1,116 @@
 /*!
  * set-value <https://github.com/jonschlinkert/set-value>
  *
- * Copyright (c) 2014-2015, 2017, Jon Schlinkert.
+ * Copyright (c) 2014-2018, Jon Schlinkert.
  * Released under the MIT License.
  */
 
 'use strict';
 
-var split = require('split-string');
-var extend = require('extend-shallow');
-var isPlainObject = require('is-plain-object');
-var isObject = require('is-extendable');
+const isPlain = require('is-plain-object');
 
-module.exports = function(obj, prop, val) {
-  if (!isObject(obj)) {
-    return obj;
+function set(target, path, value, options) {
+  if (!isObject(target)) {
+    return target;
   }
 
-  if (Array.isArray(prop)) {
-    prop = [].concat.apply([], prop).join('.');
+  let opts = options || {};
+  const isArray = Array.isArray(path);
+  if (!isArray && typeof path !== 'string') {
+    return target;
   }
 
-  if (typeof prop !== 'string') {
-    return obj;
+  let merge = opts.merge;
+  if (merge && typeof merge !== 'function') {
+    merge = Object.assign;
   }
 
-  var keys = split(prop, {sep: '.', brackets: true});
-  var len = keys.length;
-  var idx = -1;
-  var current = obj;
+  const keys = isArray ? path : split(path, opts);
+  const len = keys.length;
+  const orig = target;
 
-  while (++idx < len) {
-    var key = keys[idx];
-    if (idx !== len - 1) {
-      if (!isObject(current[key])) {
-        current[key] = {};
-      }
-      current = current[key];
-      continue;
+  if (!options && keys.length === 1) {
+    result(target, keys[0], value, merge);
+    return target;
+  }
+
+  for (let i = 0; i < len; i++) {
+    let prop = keys[i];
+
+    if (!isObject(target[prop])) {
+      target[prop] = {};
     }
 
-    if (isPlainObject(current[key]) && isPlainObject(val)) {
-      current[key] = extend({}, current[key], val);
-    } else {
-      current[key] = val;
+    if (i === len - 1) {
+      result(target, prop, value, merge);
+      break;
     }
+
+    target = target[prop];
   }
 
-  return obj;
-};
+  return orig;
+}
+
+function result(target, path, value, merge) {
+  if (merge && isPlain(target[path]) && isPlain(value)) {
+    target[path] = merge({}, target[path], value);
+  } else {
+    target[path] = value;
+  }
+}
+
+function split(path, options) {
+  const id = createKey(path, options);
+  if (set.memo[id]) return set.memo[id];
+
+  const char = (options && options.separator) ? options.separator : '.';
+  let keys = [];
+  let res = [];
+
+  if (options && typeof options.split === 'function') {
+    keys = options.split(path);
+  } else {
+    keys = path.split(char);
+  }
+
+  for (let i = 0; i < keys.length; i++) {
+    let prop = keys[i];
+    while (prop && prop.slice(-1) === '\\' && keys[i + 1]) {
+      prop = prop.slice(0, -1) + char + keys[++i];
+    }
+    res.push(prop);
+  }
+  set.memo[id] = res;
+  return res;
+}
+
+function createKey(pattern, options) {
+  let id = pattern;
+  if (typeof options === 'undefined') {
+    return id + '';
+  }
+  const keys = Object.keys(options);
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    id += ';' + key + '=' + String(options[key]);
+  }
+  return id;
+}
+
+function isObject(val) {
+  switch (typeof val) {
+    case 'null':
+      return false;
+    case 'object':
+      return true;
+    case 'function':
+      return true;
+    default: {
+      return false;
+    }
+  }
+}
+
+set.memo = {};
+module.exports = set;
--- test.js
+++ test.js
@@ -1,54 +1,55 @@
 /*!
  * set-value <https://github.com/jonschlinkert/set-value>
  *
- * Copyright (c) 2014-2017, Jon Schlinkert.
+ * Copyright (c) 2014-2018, Jon Schlinkert.
  * Released under the MIT License.
  */
 
 'use strict';
 
 require('mocha');
-var assert = require('assert');
-var set = require('./');
+const split = require('split-string');
+const assert = require('assert');
+const set = require('./');
 
 describe('set', function() {
   it('should return non-objects', function() {
-    var res = set('foo', 'a.b', 'c');
+    const res = set('foo', 'a.b', 'c');
     assert.equal(res, 'foo');
   });
 
-  it('should create a nested property if it does not already exist:', function() {
-    var o = {};
+  it('should create a nested property if it does not already exist', function() {
+    const o = {};
     set(o, 'a.b', 'c');
     assert.equal(o.a.b, 'c');
   });
 
-  it('should extend an existing value:', function() {
+  it('should merge an existing value with the given value', function() {
     var o = {a: {b: {c: 'd'}}};
-    set(o, 'a.b', {y: 'z'});
+    set(o, 'a.b', {y: 'z'}, { merge: true });
     assert.deepEqual(o.a.b, {c: 'd', y: 'z'});
   });
 
-  it('should update an object value:', function() {
+  it('should update an object value', function() {
     var o = {};
     set(o, 'a', {b: 'c'});
-    set(o, 'a', {c: 'd'});
+    set(o, 'a', {c: 'd'}, { merge: true });
     assert.deepEqual(o, {a: {b: 'c', c: 'd'}});
     set(o, 'a', 'b');
     assert.equal(o.a, 'b');
   });
 
-  it('should extend an array:', function() {
-    var o = {a: []};
+  it('should extend an array', function() {
+    const o = {a: []};
     set(o, 'a.0', {y: 'z'});
     assert(Array.isArray(o.a));
     assert.deepEqual(o.a[0], {y: 'z'});
   });
 
-  it('should extend a function:', function() {
+  it('should extend a function', function() {
     function log() {}
-    var warning = function() {};
-    var o = {};
+    const warning = function() {};
+    const o = {};
 
     set(o, 'helpers.foo', log);
     set(o, 'helpers.foo.warning', warning);
@@ -56,7 +57,7 @@ describe('set', function() {
     assert.equal(typeof o.helpers.foo.warning, 'function');
   });
 
-  it('should extend an object in an array:', function() {
+  it('should extend an object in an array', function() {
     var o = {a: [{}, {}, {}]};
     set(o, 'a.0.a', {y: 'z'});
     set(o, 'a.1.b', {y: 'z'});
@@ -67,13 +68,13 @@ describe('set', function() {
     assert.deepEqual(o.a[2].c, {y: 'z'});
   });
 
-  it('should create a deeply nested property if it does not already exist:', function() {
+  it('should create a deeply nested property if it does not already exist', function() {
     var o = {};
     set(o, 'a.b.c.d.e', 'c');
     assert.equal(o.a.b.c.d.e, 'c');
   });
 
-  it('should not create a nested property if it does already exist:', function() {
+  it('should not create a nested property if it does already exist', function() {
     var first = {name: 'Halle'};
     var o = {a: first };
     set(o, 'a.b', 'c');
@@ -82,7 +83,7 @@ describe('set', function() {
     assert.equal(o.a.name, 'Halle');
   });
 
-  it('should support immediate properties:', function() {
+  it('should support immediate properties', function() {
     var o = {};
     set(o, 'a', 'b');
     assert.equal(o.a, 'b');
@@ -96,7 +97,7 @@ describe('set', function() {
     assert.deepEqual(o, { a: {locals: {name: { first: 'Brian' }} }, b: {locals: {name: { last: 'Woodward' }} }});
   });
 
-  it('should add the property even if a value is not defined:', function() {
+  it('should add the property even if a value is not defined', function() {
     var fixture = {};
     assert.deepEqual(set(fixture, 'a.locals.name'), {a: {locals: { name: undefined }}});
     assert.deepEqual(set(fixture, 'b.locals.name'), {b: {locals: { name: undefined }}, a: {locals: { name: undefined }}});
@@ -108,11 +109,6 @@ describe('set', function() {
 
   it('should support passing an array as the key', function() {
     var actual = set({a: 'a', b: {c: 'd'}}, ['b', 'c', 'd'], 'eee');
-    assert.deepEqual(actual, {a: 'a', b: {c: {d: 'eee'}}});
-  });
-
-  it('should work with nested arrays in keys', function() {
-    var actual = set({a: 'a', b: {c: 'd'}}, [['b'], ['c', 'd']], 'eee');
     assert.deepEqual(actual, {a: 'a', b: {c: {d: 'eee'}}});
   });
 
@@ -146,55 +142,63 @@ describe('set', function() {
 });
 
 describe('escaping', function() {
-  it('should not split inside double quotes:', function() {
+  it('should not split escaped dots', function() {
     var o = {};
-    set(o, 'a."b.c.d".e', 'c');
-    assert.equal(o.a['b.c.d'].e, 'c');
-  });
-
-  it('should not split inside single quotes:', function() {
-    var o = {};
-    set(o, "a.'b.c.d'.e", 'c');
-    assert.equal(o.a['b.c.d'].e, 'c');
-  });
-
-  it('should not split inside square brackets:', function() {
-    var o = {};
-    set(o, "a.[b.c.d].e", 'c');
-    assert.equal(o.a['[b.c.d]'].e, 'c');
-  });
-
-  it('should not split inside parens:', function() {
-    var o = {};
-    set(o, "a.(b.c.d).e", 'c');
-    assert.equal(o.a['(b.c.d)'].e, 'c');
-  });
-
-  it('should not split inside angle brackets:', function() {
-    var o = {};
-    set(o, "a.<b.c.d>.e", 'c');
-    assert.equal(o.a['<b.c.d>'].e, 'c');
-  });
-
-  it('should not split inside curly braces:', function() {
-    var o = {};
-    set(o, "a.{b.c.d}.e", 'c');
-    assert.equal(o.a['{b.c.d}'].e, 'c');
-  });
-
-  it('should not split escaped dots:', function() {
-    var o = {};
-    set(o, 'a\\.b.c.d.e', 'c');
+    set(o, 'a\\.b.c.d.e', 'c', { escape: true });
     assert.equal(o['a.b'].c.d.e, 'c');
   });
 
-  it('should work with multiple escaped dots:', function() {
+  it('should work with multiple escaped dots', function() {
     var obj1 = {};
-    set(obj1, 'e\\.f\\.g', 1);
+    set(obj1, 'e\\.f\\.g', 1, { escape: true });
     assert.equal(obj1['e.f.g'], 1);
 
     var obj2 = {};
-    set(obj2, 'e\\.f.g\\.h\\.i.j', 1);
+    set(obj2, 'e\\.f.g\\.h\\.i.j', 1, { escape: true });
     assert.deepEqual(obj2, { 'e.f': { 'g.h.i': { j: 1 } } });
+  });
+});
+
+describe('options', function() {
+  const options = {
+    split: function(segment) {
+      return split(segment, { separator: '.', brackets: true });
+    }
+  };
+
+  it('should use a custom function to not split inside double quotes', function() {
+    var o = {};
+    set(o, 'a."b.c.d".e', 'c', options);
+    assert.equal(o.a['b.c.d'].e, 'c');
+  });
+
+  it('should use a custom function to not split inside single quotes', function() {
+    var o = {};
+    set(o, "a.'b.c.d'.e", 'c', options);
+    assert.equal(o.a['b.c.d'].e, 'c');
+  });
+
+  it('should use a custom function to not split inside square brackets', function() {
+    var o = {};
+    set(o, "a.[b.c.d].e", 'c', options);
+    assert.equal(o.a['[b.c.d]'].e, 'c');
+  });
+
+  it('should use a custom function to not split inside parens', function() {
+    var o = {};
+    set(o, "a.(b.c.d).e", 'c', options);
+    assert.equal(o.a['(b.c.d)'].e, 'c');
+  });
+
+  it('should use a custom function to not split inside angle brackets', function() {
+    var o = {};
+    set(o, "a.<b.c.d>.e", 'c', options);
+    assert.equal(o.a['<b.c.d>'].e, 'c');
+  });
+
+  it('should use a custom function to not split inside curly braces', function() {
+    var o = {};
+    set(o, "a.{b.c.d}.e", 'c', options);
+    assert.equal(o.a['{b.c.d}'].e, 'c');
   });
 });
