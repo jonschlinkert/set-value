3ac390947606d009c020199f9acb43748fb9f698
kebler.net <kebler.net@gmail.com>
2021-05-02 06:47:19 -0700

    allow multiple separator delimiters split string elements in an array path if requested

--- examples.js
+++ examples.js
@@ -2,39 +2,38 @@
 
 const set = require('.');
 
-const data = {};
 const key1 = Symbol('key-1');
 const key2 = Symbol('key-2');
 const key3 = Symbol('key-3');
 const key4 = Symbol('key-4');
 
-set(data, 'a.b.c', true);
-set(data, '"a.b".c', true, { split: set.parse });
-set(data, 'foo-bar', true);
-set(data, ['one', 'two'], true);
-set(data, ['one', key1], true);
-set(data, [key2, key3, key4], true);
-set(data, key1, true);
+const log = o => console.dir(o, { depth: null });
 
-console.log(data);
+log(set({}, key1, true));
+log(set({}, [key2, key3, key4], true));
+log(set({}, 'a.b.c', true));
+log(set({}, 'a\\.b\\.c.d', true));
+log(set({}, ['one.two', 'three', key1], true));
+log(set({}, ['one:two', key2, 'three'], true, { separator: ':', elSplit: true }));
+log(set({}, 'uno\\.dos%tres/quatro.cinco', true, { separator: '%./' }));
 
-// console.log(set({}, 'a.b.c', 'd'));
+// log(set({}, 'a.b.c', 'd'));
 // //=> { a: { b: { c: 'd' } } }
 
-// console.log(set({}, 'a\\.b.c', 'd'));
+// log(set({}, 'a\\.b.c', 'd'));
 // //=> { 'a.b': { c: 'd' } }
 
-// console.log(set({}, 'a\\.b\\.c', 'd'));
-// //=> { 'a.b.c': 'd' }
+// log(set({}, 'a\\.b\\.c', 'd'));
+//=> { 'a.b.c': 'd' }
 
-// console.log(set({}, '"a.b".c', 'd'));
+// log(set({}, '"a.b".c', 'd'));
 // //=> { 'a.b': { c: 'd' } }
 
-// console.log(set({}, "'a.b'.c", "d"));
+// log(set({}, "'a.b'.c", "d"));
 // //=> { 'a.b': { c: 'd' } }
 
-// console.log(set({}, "{a..b}.c", "d"));
+// log(set({}, "{a..b}.c", "d"));
 // //=> { '{a..b}': { c: 'd' } }
 
-// console.log(set({}, '"this/is/a/.file.path"', 'd'));
-// //=> { 'this/is/a/.file.path': 'd' }
+// log(set({}, '"this/is/a/.file.path"', 'd'));
+//=> { 'this/is/a/.file.path': 'd' }
